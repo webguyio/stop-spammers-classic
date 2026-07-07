e808b5a1bb1a63b11b66dbcc63a04e2762c846cd
Web Guy <webguywork@gmail.com>
2026-07-07 01:01:24 -0600

    Ver 2026.6

--- stop-spammer-registrations-new.php
+++ stop-spammer-registrations-new.php
@@ -3,8 +3,8 @@
 Plugin Name: Stop Spammers Classic
 Plugin URI: https://damspam.com/
 Description: A simplified, restored, and preserved version of the original Stop Spammers plugin.
-Version: 2026.5
-Requires at least: 3.0
+Version: 2026.6
+Requires at least: 5.0
 Requires PHP: 5.0
 Author: Web Guy
 Author URI: https://webguy.io/
@@ -18,7 +18,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 // networking requires a couple of globals
-define( 'SS_VERSION', '2026.5' );
+define( 'SS_VERSION', '2026.6' );
 define( 'SS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SS_PLUGIN_FILE', plugin_dir_path( __FILE__ ) );
 define( 'SS_PLUGIN_DATA', wp_upload_dir()['basedir'] . '/data/' );
@@ -57,7 +57,8 @@ function ss_admin_notice() {
 	if ( !get_user_meta( $user_id, 'ss_notice_dismissed_' . $version_key ) && current_user_can( 'manage_options' ) ) {
 		$admin_url = esc_url( admin_url( add_query_arg( array(), '' ) ) );
 		$param = strpos( $admin_url, '?' ) !== false ? '&' : '?';
-		echo wp_kses_post( sprintf( '<div class="notice notice-warning"><p><a href="%s%sdismiss&_wpnonce=%s" class="alignright" style="text-decoration:none"><big>✕</big></a><big><strong>Stop Spammers is now Dam Spam</strong></big><p><a href="https://wordpress.org/plugins/dam-spam/" class="button-primary" style="border-color:#4aa863;background:#4aa863" target="_blank">Make the Switch</a> &nbsp; <a href="https://github.com/webguyio/dam-spam/issues" class="button-primary" target="_blank">Get Help</a></p><p>Dam Spam is the modern version of Stop Spammers where all future development will take place. The core features remain the same, but some bloated and dated features have been removed and some new smart features have been added. Please join the discussion on GitHub (github.com/webguyio/dam-spam/issues) if you have any questions or suggestions. All of your settings will automatically migrate over in the background and still be there if you decide to switch back. While migrating is optional, it\'s strongly recommended.</p></div>', esc_url( $admin_url ), esc_html( $param ), esc_attr( wp_create_nonce( 'ss_dismiss_notice' ) ) ) );
+		$install_url = esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=dam-spam' ), 'install-plugin_dam-spam' ) );
+		echo wp_kses_post( sprintf( '<div class="notice notice-error"><p><a href="%s%sdismiss&_wpnonce=%s" class="alignright" style="text-decoration:none"><big>✕</big></a><big><strong>Stop Spammers is now Dam Spam</strong></big><p><a href="%s" class="button-primary" style="border-color:#4aa863;background:#4aa863">Install Dam Spam Now</a> &nbsp; <a href="mailto:webguywork@gmail.com" class="button-primary" target="_blank">Get Help</a></p><p>Development for Stop Spammers has ended. It\'s legacy code that is severely dated and unoptimized. Moving forward, it will receive security patches only. For continued development, modernized code, new and improved features, you\'ll need to migrate to Dam Spam. Doing so is extremely easy. Simply install and activate it and all your settings will automatically migrate over in the background.</p></div>', esc_url( $admin_url ), esc_html( $param ), esc_attr( wp_create_nonce( 'ss_dismiss_notice' ) ), $install_url ) );
 	}
 }
 add_action( 'admin_notices', 'ss_admin_notice' );
